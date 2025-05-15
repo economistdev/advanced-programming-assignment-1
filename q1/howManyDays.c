ff607807a3255d79dc140fa1e5ece2fb5a7ca231
vboxuser <georgecross98@gmail.com>
2025-05-15 14:13:28 +0000

    first

---
+++ q1/howManyDays.c
@@ -0,0 +1,20 @@
+
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+
+int main() {
+
+
+
+	return 0;
+}
+
+struct Date {
+	int day;
+	int month;
+	int year;
+};
+
+bool
