d07b300ff6c093cf25b33eb891281ebe2ee19927
vboxuser <georgecross98@gmail.com>
2025-05-22 13:52:52 +0000

    stsarted q2

---
+++ q2/removeDigit.c
@@ -0,0 +1,38 @@
+
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+
+bool checkDigit() {
+
+	
+}
+
+
+void recursiveSolve(int *solution_addr, int x, int n, int curr_a) {
+
+	int max_a = n/curr_a;
+
+	
+}
+
+void removeDigit(int x, int n, int *a, int *b) {
+
+
+}
+
+void test() {
+
+
+
+}
+
+int main() {
+	
+	printf("%d\n", howManyDays());
+	//test();
+	
+	return 0;
+}	
+
