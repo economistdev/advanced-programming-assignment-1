31d920064672daceef0ed4e2972e5b35cdd52bca
vboxuser <georgecross98@gmail.com>
2025-05-26 13:54:51 +0000

    finished linked list q3

--- q3/redact_words.c
+++ q3/redact_words.c
@@ -33,14 +33,21 @@ void add_redact_word(struct RedactList list, const char *word) {
 	if (list.head == NULL) {
 		list.head = new_node;
 	} else {
-		struct RedactNode *curr_node
+		struct RedactNode *curr_node = list.head;
+		while (curr_node->next != NULL) {
+			curr_node = curr_node->next;
+		}
+		curr_node->next = new_node;
 	}
-
-
 }
 
-bool should_redact_word(const char *word) {
-
+bool should_redact_word(struct RedactList list, const char *word) {
+	struct RedactNode *curr_node = list.head;
+	while (curr_node != NULL) {
+		if (strcasecmp(curr_node->word, word) == 0) return true;
+		curr_node = curr_node->next;
+	}
+	return false;
 }
 
 void redact_words(const char *text_filename, const char *redact_words_filename) {
